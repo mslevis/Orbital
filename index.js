95bb6ec64d18a9f084d7106ef4b6de2ded742cab
mslevis <tanteongyuowen@gmail.com>
2021-05-31 13:20:04 +0800

    test

--- index.js
+++ index.js
@@ -160,9 +160,11 @@ if (process.env.NODE_ENV === "production") {
         }
     });
 
+    /*
     app.get("*", (req, res) => {
         res.sendFile(path.join(__dirname, "client/build/index.html"))
     });
+    */
 
 app.listen(PORT, () => {
     console.log(`server has started on port ${PORT}`);
