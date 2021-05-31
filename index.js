c0a08db032410631ebb5eaeb1027cb6336f23ab5
mslevis <tanteongyuowen@gmail.com>
2021-05-31 17:20:18 +0800

    test

--- index.js
+++ index.js
@@ -13,7 +13,7 @@ app.use(cors());
 app.use(express.json());
 
 
-if (process.env.NODE_ENV === "production") {
+if (p\qrocess.env.NODE_ENV === "production") {
     //server static content
     //npm run build
     app.use(express.static(path.join(__dirname, "./client/build")))
